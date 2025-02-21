f808e09d7da6f342532d712d817635b1a9e44a04
ADIB3883 <adibhoq55@gmail.com>
2025-02-22 00:03:32 +0600

    Latest update by Adib

---
+++ src/main/java/me/hawkease/requestforaplace.java
@@ -0,0 +1,18 @@
+package me.hawkease;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class requestforaplace {
+
+    @FXML
+    void open_map(MouseEvent event) {
+
+    }
+
+    @FXML
+    void requestt(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/me/hawkease/sellerpage.java
@@ -0,0 +1,37 @@
+package me.hawkease;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+public class sellerpage {
+
+    @FXML
+    private StackPane page;
+
+    @FXML
+    void log_out(MouseEvent event) {
+
+    }
+
+    @FXML
+    void open_profile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void pay_bill(MouseEvent event) {
+
+    }
+
+    @FXML
+    void request_for_a_place(MouseEvent event) {
+
+    }
+
+    @FXML
+    void add_products(MouseEvent event) {
+
+    }
+
+}
