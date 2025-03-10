ddf628e75d99f67e94ec23402eddef5f6889270c
ADIB3883 <adibhoq55@gmail.com>
2025-03-10 19:40:11 +0600

    Adib arki

--- src/main/java/me/hawkease/main_class.java
+++ src/main/java/me/hawkease/main_class.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class main_class extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(main_class.class.getResource("profile.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(main_class.class.getResource("welcome_page.fxml"));
         // current_user.get_user().set_email("mdragibhossain09@gmail.com");
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("HawkEase!");
